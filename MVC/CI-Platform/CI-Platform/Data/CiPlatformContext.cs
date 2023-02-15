e90299efd4318033cc4e5f8d4e41880e9b1d3369
het <hetshah2207@gmail.com>
2023-02-15 17:38:32 +0530

    new commit

---
+++ MVC/CI-Platform/CI-Platform/Data/CiPlatformContext.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using CI_Platform.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CI_Platform.Data;
+
+public partial class CiPlatformContext : DbContext
+{
+    public CiPlatformContext()
+    {
+    }
+
+    public CiPlatformContext(DbContextOptions<CiPlatformContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.;; initial catalog=CI-Platform;Encrypt=False;Trusted_Connection=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>(entity =>
+        {
+            entity.HasKey(e => e.CityId).HasName("PK__city__031491A8F10DFD08");
+
+            entity.ToTable("city");
+
+            entity.Property(e => e.CityId).HasColumnName("city_id");
+            entity.Property(e => e.CountryId).HasColumnName("country_id");
+            entity.Property(e => e.CreatedAt)
+                .IsRowVersion()
+                .IsConcurrencyToken()
+                .HasColumnName("created_at");
+            entity.Property(e => e.DeletedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("deleted_at");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("updated_at");
+
+            entity.HasOne(d => d.Country).WithMany(p => p.Cities)
+                .HasForeignKey(d => d.CountryId)
+                .HasConstraintName("FK__city__country_id__3D5E1FD2");
+        });
+
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasKey(e => e.CountryId).HasName("PK__country__7E8CD055A8D2E6A3");
+
+            entity.ToTable("country");
+
+            entity.Property(e => e.CountryId).HasColumnName("country_id");
+            entity.Property(e => e.CreatedAt)
+                .IsRowVersion()
+                .IsConcurrencyToken()
+                .HasColumnName("created_at");
+            entity.Property(e => e.DeletedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("deleted_at");
+            entity.Property(e => e.Iso)
+                .HasMaxLength(16)
+                .IsUnicode(false)
+                .HasColumnName("ISO");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("name");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("updated_at");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__user__B9BE370F7C290218");
+
+            entity.ToTable("user");
+
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+            entity.Property(e => e.Avatar)
+                .HasMaxLength(2048)
+                .IsUnicode(false)
+                .HasColumnName("avatar");
+            entity.Property(e => e.CityId).HasColumnName("city_id");
+            entity.Property(e => e.CountryId).HasColumnName("country_id");
+            entity.Property(e => e.CreatedAt)
+                .IsRowVersion()
+                .IsConcurrencyToken()
+                .HasColumnName("created_at");
+            entity.Property(e => e.DeletedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("deleted_at");
+            entity.Property(e => e.Department)
+                .HasMaxLength(16)
+                .IsUnicode(false)
+                .HasColumnName("department");
+            entity.Property(e => e.Email)
+                .HasMaxLength(128)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.EmployeeId)
+                .HasMaxLength(16)
+                .IsUnicode(false)
+                .HasColumnName("employee_id");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(16)
+                .IsUnicode(false)
+                .HasColumnName("first_name");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(16)
+                .IsUnicode(false)
+                .HasColumnName("last_name");
+            entity.Property(e => e.LinkedInUrl)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("linked_in_url");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("password");
+            entity.Property(e => e.PhoneNumber).HasColumnName("phone_number");
+            entity.Property(e => e.ProfileText)
+                .HasColumnType("text")
+                .HasColumnName("profile_text");
+            entity.Property(e => e.Status)
+                .IsRequired()
+                .HasDefaultValueSql("((1))")
+                .HasColumnName("status");
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("title");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("updated_at");
+            entity.Property(e => e.WhyIVolunteer)
+                .HasColumnType("text")
+                .HasColumnName("why_i_volunteer");
+
+            entity.HasOne(d => d.City).WithMany(p => p.Users)
+                .HasForeignKey(d => d.CityId)
+                .HasConstraintName("FK__user__city_id__47DBAE45");
+
+            entity.HasOne(d => d.Country).WithMany(p => p.Users)
+                .HasForeignKey(d => d.CountryId)
+                .HasConstraintName("FK__user__country_id__48CFD27E");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MVC/CI-Platform/CI-Platform/Models/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CI_Platform.Models;
+
+public partial class User
+{
+    public long UserId { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int PhoneNumber { get; set; }
+
+    public string? Avatar { get; set; }
+
+    public string? WhyIVolunteer { get; set; }
+
+    public string? EmployeeId { get; set; }
+
+    public string? Department { get; set; }
+
+    public long? CityId { get; set; }
+
+    public long? CountryId { get; set; }
+
+    public string? ProfileText { get; set; }
+
+    public string? LinkedInUrl { get; set; }
+
+    public string? Title { get; set; }
+
+    public bool? Status { get; set; }
+
+    public byte[] CreatedAt { get; set; } = null!;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public virtual City? City { get; set; }
+
+    public virtual Country? Country { get; set; }
+}
